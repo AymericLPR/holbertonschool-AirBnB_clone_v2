f03883679364d92acfabe1144b228ba5e5ccda83
AymericLPR <mric31400@gmail.com>
2023-04-05 03:48:13 -0700

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ print text input """
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -37,10 +37,7 @@ def numberonly(n):
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def numbertemplate(n):
-    if isinstance(n, int):
-        return render_template("5-number.html", n=n)
-    else:
-        return
+    return render_template('5-number.html', number=n)
 
 
 if __name__ == "__main__":
