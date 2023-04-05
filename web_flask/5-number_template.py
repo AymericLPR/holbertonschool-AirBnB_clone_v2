005bf46e9a1535ffdf079dd7727c47f2020efe15
AymericLPR <mric31400@gmail.com>
2023-04-05 03:19:33 -0700

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,7 +38,7 @@ def numberonly(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def numbertemplate(n):
     if isinstance(n, int):
-        return render_template("5-number.html", number=n)
+        return render_template("5-number.html", n=n)
     else:
         return
 
